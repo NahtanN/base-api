f213a5cc7ab60311cd14345c15b1bb0d57f060eb
Nathan Gomes <nathan.neri@al.senai.br>
2024-07-09 21:18:00 -0300

    config files added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./application/app.module";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT, () =>
+    Logger.log(
+      `Application running on ${process.env.APP_URL}:${process.env.PORT}`,
+    ),
+  );
 }
 bootstrap();
